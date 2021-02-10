463caab8c5fce025aced19808f1f98d722d0404e
evgeniailio <78660810+evgeniailio@users.noreply.github.com>
2021-02-10 13:33:46 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,24 @@
-
 makeCacheMatrix<-function(x=matrix()){
-     
-     #set inv as null
-     inv<-NULL
-     
-     #assign a new value y 
-     set<-function(y){
-          x<<-y
-          inv<<-NULL
-          
-     }
-     
-     #get the value of matrix
-     get<-function() (x)
-     
-     #define setinverse  
-     setinverse<-function(inverse)  (inv <<- inverse)
-     
-     #it gets the value of inverse matrix
-     getinverse<-function() (inv)
-     
-     list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+        
+        #set inv as null
+        inv<-NULL
+        
+        #assign a new value y 
+        set<-function(y){
+                x<<-y
+                inv<<-NULL
+                
+        }
+        
+        
+        #get the value of matrix
+        get<-function() (x)
+        
+        #define setinverse  
+        setinverse<-function(inverse)  (inv <<- inverse)
+        
+        #it gets the value of inverse matrix
+        getinverse<-function() (inv)
+        
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
